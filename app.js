86a934ef1969440968dc7aec140a55e6ae3c8fc6
Rasutei <rasutei@outlook.com>
2022-09-26 15:44:56 -0300

    Fully working, in theory

--- app.js
+++ app.js
@@ -7,69 +7,101 @@ const $chat = $('#messages')
 const $skip = $('#voteskip')
 const $req_inp = $('#request-input')
 const $req_snd = $('#send-request')
+const $vol = $('#volume')
+const $vid_title = $('#video-title')
 
 // -------------------------------------------- //
 // ----------------- WebSocket ---------------- //
 // -------------------------------------------- //
+const DEFAULT_COLOR = 'rgb(0, 183, 255)'
 let token = (localStorage.chat_token) ? localStorage.chat_token : 'unreceived'
 let username
-let color
+let color = DEFAULT_COLOR
 
 //* WS response template
 class WSR {
 	token = token
 	username = username? username : 'undefined'
 	color = color? color : ''
-	content
 
-	/**
-	 * WebSocket server response template
-	 * @param {string} type 
-	 * @param {object} content 
-	 */
-	constructor(content = '') {
-		this.content = content
+	constructor(content=undefined) {
+		if (content == undefined) return
+		Object.keys(content).forEach((k) => this[k] = content[k])
 	}
 
-	get string() {return JSON.stringify(this)}
+	toString() {
+		if (this.username == 'undefined') return
+		return JSON.stringify(this)
+	}
 }
 //* Chat handling
 let ws
 setInterval(() => {
 	if (ws == undefined) {
-		ws = new WebSocket('ws://localhost:8000/')
+		ws = new WebSocket(window.location.href.replace('https', 'wss').replace('http', 'ws'))
 		ws.onopen = function(ev) {
 			console.log('System: Chat WebSocket connected.')
-			ws.send(new WSR().string)
-			$([$cht_inp[0], $cht_snd[0], $skip[0], $req_inp[0], $req_snd[0]])
-				.removeAttr('disabled')
+			ws.send(JSON.stringify(new WSR()))
+			$([$cht_inp[0], $cht_snd[0],]).removeAttr('disabled')
+			if (username != undefined)
+			$([$req_inp[0], $req_snd[0], $skip[0]]).removeAttr('disabled')
+			else if (localStorage.username)
+			$cht_inp.val(localStorage.username)
 			$cht_inp.attr('placeholder', username == undefined? 'Set a username' : 'Type a message')
 			$req_inp.attr('placeholder', username == undefined? 'Set a username first' : 'Insert a link, video ID, or search string')
 			$(document.body).removeClass('disconnected')
 		}
 		ws.onclose = function() {
-			$cht_snd.attr('disabled', '')
-			$cht_inp.attr('disabled', '')
-				.attr('placeholder', 'Disconnected. Retrying...')
+			$([$cht_inp[0], $cht_snd[0], $skip[0], $req_inp[0], $req_snd[0]])
+			.attr('disabled', '')
+			$cht_inp.attr('placeholder', 'Disconnected. Retrying...')
 			$(document.body).addClass('disconnected')
 			console.log('System: Chat WebSocket disconnected. Retrying...')
 			ws = undefined
 		}
 		ws.onmessage = function(e) {
 			const msg = JSON.parse(e.data)
-			console.log('System: Received:')
 			console.log(msg)
+
 			if (msg.type == 'token') {
 				console.log('System: Received and stored session token')
 				token = msg.token
 				return
 			}
 			if (msg.type == 'message')
-				AddMessage(msg.username, msg.content, msg.order, msg.color)
+				AddMessage(msg.username, msg.message, msg.order, msg.color)
 			if (msg.type == 'history') {
 				$chat.children().filter((_, e) => Array.from(e.classList).includes('innate') == false).remove()
-				msg.history.forEach((m) => AddMessage(m.username, m.content, m.order, m.color))
+				msg.history.forEach((m) => AddMessage(m.username, m.message, m.order, m.color))
 			}
+			if (msg.type == 'confirmvote')
+				msg.vote? $skip.addClass('voted') : $skip.removeClass('voted')
+			if (msg.type == 'video') {
+				$skip.removeClass('voted')
+				$vid_title.html(msg.title)
+				player.cueVideoById(msg.id)
+				start_time = msg.start_time
+				console.log(`Player: Delayed by ${(Date.now() - msg.start_time)/1000}`)
+				const playvideo = setInterval(() => {
+					player.seekTo((Date.now() - msg.start_time) / 1000, true)
+					player.playVideo()
+					$skip.removeClass('voted')
+					console.log('Trying to play bcause video arrived')
+					if (player.getPlayerState() == PLAYING)
+						clearInterval(playvideo)
+				}, 50)
+			}
+			if (msg.type == 'player')
+				switch (msg.order) {
+					case PLAYING:
+						player.playVideo()
+						break
+					case ENDED:
+						player.pauseVideo()
+				}
+			// if (msg.type == 'catchup')
+				// if (Math.abs(Date.now() - (msg.start_time + player.getCurrentTime()*1000)) > 300)
+				// 	player.seekTo((Date.now() - start_time) / 1000, true)
 		}
 	}
 }, 1000)
@@ -77,14 +109,69 @@ setInterval(() => {
 // -------------------------------------------- //
 // ------------------- Setup ------------------ //
 // -------------------------------------------- //
+//* Event handlers
 $cht_snd.on('click', SendMessage)
 $cht_inp.on('keypress', (e) => {if (e.key == 'Enter') $cht_snd.trigger('click')})
+$req_snd.on('click', SendRequest)
+$req_inp.on('keypress', (e) => {if (e.key == 'Enter') $req_snd.trigger('click')})
+$vol.on('input', function(){
+	player.setVolume(+this.value)
+	$vol.next().css('width', `${this.value}%`)
+})
+$skip.on('click', SendVoteskip)
 
+//* Layout change listeners
 let mobile_mode = window.matchMedia("(max-width: 600px)").matches
 window.matchMedia("(max-width: 600px)").addEventListener('change', (e) => {
 	mobile_mode = e.matches
 	ScrollChat()
 })
+
+//* Player
+const ENDED = 0
+const PLAYING = 1
+const PAUSED = 2
+const BUFFERING = 3
+const CUED = 5
+const PlayerState = {
+	0: 'ENDED',
+	1: 'PLAYING',
+	2: 'PAUSED',
+	3: 'BUFFERING',
+	5: 'CUED',
+}
+let player
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		playerVars: {
+			'autoplay': 1,
+			'playsinline': 1,
+			'controls': 0,
+			'disablekb': 1,
+			'fs': 0,
+			'iv_load_policy': 3,
+			'modestbranding': 1,
+		},
+		events: {
+			'onPlayerReady ': (e) => player.playVideo(),
+			'onStateChange': (e) => {
+				if (!username) return
+				if (e.data == CUED ||/* e.data == PLAYING || e.data == BUFFERING ||*/ e.data == ENDED) {
+					ws.send(new WSR({playstate: PlayerState[e.data]}))
+				}
+			}
+		}
+	})
+}
+
+//* Player auto catch-up
+let start_time
+setInterval(() => {
+	if (player.getPlayerState() != PLAYING) return
+	if (Math.abs(Date.now() - (start_time + player.getCurrentTime()*1000)) > 300)
+		player.seekTo((Date.now() - start_time) / 1000, true)
+}, 1000)
 
 // -------------------------------------------- //
 // ----------------- Functions ---------------- //
@@ -94,28 +181,42 @@ function SendMessage() {
 	if (msg.length == 0) return
 	if (!username) {
 		username = msg
+		localStorage.setItem('username', msg)
 		$cht_inp.attr('placeholder', 'Type a message')
 		$req_inp.attr('placeholder', 'Insert a link, video ID, or search string')
+		$([$req_inp[0], $req_snd[0], $skip[0]]).removeAttr('disabled')
 		$cht_inp.val('')
 		return
 	}
+	if (msg == 'color=')
+		color = DEFAULT_COLOR
 	if (msg.slice(0, 6) == 'color=') {
 		color = msg.slice(6)
 		$cht_inp.val('')
 		return
 	}
-	ws.send(new WSR(msg).string)
+	ws.send(new WSR({message: msg}))
 	$cht_inp.val('')
 }
-function AddMessage(user='<<System>>', content='', order=0, color='') {
+function SendRequest() {
+	const query = $req_inp.val().trim().replaceAll(/\s+/g, ' ')
+	ws.send(new WSR({query: query}))
+	$req_inp.val('')
+}
+function SendVoteskip() {
+	const vote = !$skip.hasClass('voted')
+	ws.send(new WSR({vote: vote}))
+}
+function AddMessage(user, content, order=0, color='') {
 	const doScroll = Math.abs($chat[0].scrollTop) + Math.abs($chat[0].clientHeight) == $chat[0].scrollHeight
+	content = content.replaceAll(/\s+/g, ' ').replaceAll('\n', '<br>').trim()
 	if (user == '<<System>>')
 		$chat.append(
 			$('<div class="message sysmsg"></div>')
 				.css('order', order)
 				.append(
 					$('<span></span>')
-						.text(content)
+						.html(content)
 				)
 		)
 	else
--- server.js
+++ server.js
@@ -25,14 +25,18 @@ const LOG_CHT = `${LOG_CHT_O}:${LOG_RESET}`
 
 const host = 'localhost'
 const port_server = 8000
-const port_ws = 8001
+
+const ENDED = 0
+const PLAYING = 1
+const PAUSED = 2
+const BUFFERING = 3
+const CUED = 5
 
 // -------------------------------------------- //
 // --------------- Server stuff --------------- //
 //* HTTP server
 const server = http.createServer((req, res) => {
-	if (req.url == '/')
-		console.log(`${LOG_SRV} Page request`)
+	console.log(`${LOG_SRV} [${req.remoteAddress}] ${req.method} ${req.url}`)
 	if (req.method == 'POST') {
 		let body = ''
 		req.on('data', chunk => body += chunk)
@@ -113,45 +117,93 @@ class WSR {
 	 * @param {string} type 
 	 * @param {object} content 
 	 */
-	constructor(type, content) {
+	constructor(type, content={}) {
 		this.content = content
 		this.content.type = type
 	}
 
-	get string() {return JSON.stringify(this.content)}
+	toString() {
+		return JSON.stringify(this.content)
+	}
 }
 
 //* WebSocket server
 let message_list = []
+let clients_ready = []
+let clients_done = []
 let msg_index = 0
+
 const wsserver = new WebSocketServer({httpServer: server})
 wsserver.on('request', function(request) {
 	const connection = request.accept(null, request.origin)
 	console.log(`${LOG_CHT} [${request.remoteAddress}] has connected`)
-	connection.sendUTF(new WSR('history', {history: message_list}).string)
+	//- Send chat history
+	connection.sendUTF(new WSR('history', {history: message_list}))
+	//- Send current video, if there is one
+	if (queue.current)
+		connection.sendUTF(new WSR('video', queue.current))
 
-	connection.on('message', (message) => {
+	connection.on('message', async (message) => {
+		// console.log(message.utf8Data)
 		try {
 			const msg = JSON.parse(message.utf8Data)
-			console.log(`${LOG_CHT} ${message.utf8Data}`)
 
-			if (msg.token == 'unreceived') {
-				connection.sendUTF(new WSR('token', {token: request.key}).string)
-				return
-			}
-			if (msg.content) {
+			if (msg.token == 'unreceived')
+				connection.sendUTF(new WSR('token', {token: request.key}))
+			else if (msg.message) {
+				if (msg.message == '##start') {
+					queue.Dequeue()
+					return
+				}
 				const message_object = {
 					username: msg.username,
-					content: msg.content,
+					message: msg.message,
 					order: msg_index++,
 					color: msg.color
 				}
 				message_list.push(message_object)
-				wsserver.connections.forEach(c => c.sendUTF(new WSR('message', message_object).string))
+				broadcast(new WSR('message', message_object))
+				console.log(`${LOG_CHT_O}:\u001b[0;34m ${msg.username}@${connection.remoteAddress}:${LOG_RESET} ${msg.message}`)
 			}
-		} catch (e) {console.error(e)}
+			else if (msg.query) {
+				const result = await SearchYouTube(msg.query)
+				queue.Queue(new QueueItem(msg.username, msg.color, result.title, result.id))
+				SendSystemMessage(`${msg.color?`<span style="color:${msg.color}">`:''}[${msg.username}]${msg.color?'</span>':''} added [${result.title}] to the queue`)
+				console.log(`${LOG_SYS} [${msg.username}] added [${result.title}] to the queue`)
+			}
+			else if (msg.vote != undefined) {
+				queue.Voteskip(msg.username, msg.vote, msg.color)
+				connection.sendUTF(new WSR('confirmvote', {vote: msg.vote}))
+			}
+			else if (msg.playstate) {
+				console.log(`${LOG_CHT} ${msg.username}@${request.remoteAddress} ${msg.playstate}`)
+				switch (msg.playstate) {
+					case 'CUED':
+						clients_ready.push(msg.username)
+						console.log(`${LOG_SYS} Clients ready out of ${chatter_count()}: ${JSON.stringify(clients_ready)}`)
+						if (clients_ready.length == wsserver.connections.length) {
+							clients_ready = []
+							broadcast(new WSR('player', {order: PLAYING}))
+						}
+						break
+					case 'PLAYING':
+						connection.sendUTF(new WSR('catchup', {start: queue.current.start_time}))
+						break
+					case 'ENDED':
+						clients_done.push(msg.username)
+						console.log(`${LOG_SYS} Clients done out of ${chatter_count()}: ${JSON.stringify(clients_done)}`)
+						if (clients_done.length == 1)
+							SendSystemMessage(`That was [${queue.current.title}], requested by ${msg.color?`<span style="color:${msg.color}">`:''}[${queue.current.requester}]${msg.color?'</span>':''}!${queue.empty? '' : '\nNext will be cued once all have finished'}`)
+						if (clients_done.length == wsserver.connections.length) {
+							clients_done = []
+							queue.Dequeue()
+						}
+						break
+				}
+			}
+		} catch (e) {console.error(e); console.log(message)}
 	})
-	connection.on('close', (reasonCode, description) => console.log(`${LOG_CHT} [${request.remoteAddress}] has connected`))
+	connection.on('close', (reasonCode, description) => console.log(`${LOG_CHT} [${request.remoteAddress}] has disconnected`))
 })
 
 // -------------------------------------------- //
@@ -160,11 +212,13 @@ wsserver.on('request', function(request) {
 //* Classes
 class QueueItem {
 	requester
+	requesterColor
 	title
 	id
 	
-	constructor(requester, title, id) {
+	constructor(requester, color, title, id) {
 		this.requester = requester
+		this.requesterColor = color
 		this.title = title
 		this.id = id
 	}
@@ -176,11 +230,15 @@ class QueueItem {
 class Queue {
 	list = []
 	current
+	current_start
 	skipvotes = []
 	skip_threshold = 0.5
 
 	get votes_needed() {
 		return Math.max(1, Math.floor(chatter_count() * queue.skip_threshold))
+	}
+	get empty() {
+		return this.list.length == 0
 	}
 
 	/**
@@ -194,17 +252,21 @@ class Queue {
 		return size - this.list.length
 	}
 	/**
-	 * Adds item to the end of the queue and returns the length of the queue
+	 * Adds item to the end of the queue, and dequeues it if it's the only one
 	 * @param {QueueItem} item Item to be added to the queue
 	 */
 	Queue(item) {
-		return this.list.push(item)
+		this.list.push(item)
 	}
 	/**
-	 * Dequeues first item in the list and returns it
+	 * Sends next video on queue out to be played
 	 */
 	Dequeue() {
-		return this.current = this.list.shift()
+		if (this.empty) return
+		this.current = this.list.shift()
+		this.current.start_time = Date.now()
+		broadcast(new WSR('video', this.current))
+		SendSystemMessage(`Cuing [${this.current.title}], requested by ${this.current.requesterColor?`<span style="color:${this.current.requesterColor}">`:''}[${this.current.requester}]${this.current.requesterColor?'</span>':''}`)
 	}
 	/**
 	 * Resets the queue
@@ -214,14 +276,22 @@ class Queue {
 		this.skipvotes = []
 		this.skip_threshold = 0.5
 	}
-	Voteskip(username) {
-		if (this.skipvotes.includes(username)) return
-		this.skipvotes.push(username)
+	Voteskip(username, vote, color='') {
+		if (vote == true && this.skipvotes.includes(username)) return
+		console.log(queue.skipvotes)
+		if (vote == true)
+			this.skipvotes.push(username)
+		else if (vote == false)
+			this.skipvotes = this.skipvotes.filter(u => u != username)
+
 		if (this.skipvotes.length < this.votes_needed)
-		Action(`Skip votes: ${this.skipvotes}/${votes_needed()}`)
+			SendSystemMessage(`${color.length?`<span style="color:${color}">`:''}[${username}]${color.length?'</span>':''} has ${vote? 'voted to skip' : 'removed their vote'}.\nSkip votes: ${this.skipvotes.length}/${this.votes_needed}`)
 		else {
-			Action(`Skipping`)
-			this.Dequeue()
+			SendSystemMessage(`${color.length?`<span style="color:${color}">`:''}[${username}]${color.length?'</span>':''} has ${vote? 'voted to skip' : 'removed their vote'}.\nSkipping`)
+			if (this.empty)
+				broadcast(new WSR('player', {order: ENDED}))
+			else
+				this.Dequeue()
 		}
 	}
 }
@@ -242,4 +312,24 @@ async function SearchYouTube(query) {
 		const title = full.snippet.title.replaceAll('&#39;', '\'')
 		return { id: id, title: title, full: full }
 	}
+}
+/**
+ * Sends stringified message to all WebSocket clients
+ * @param {stringified} content Stringified message
+ */
+function broadcast(content) {
+	wsserver.connections.forEach(c => c.sendUTF(content))
+}
+/**
+ * Sends a chat message as the system to all clients
+ * @param {string} msg Message
+ */
+function SendSystemMessage(msg) {
+	const message = {
+		username: '<<System>>',
+		message: msg,
+		order: msg_index++,
+	}
+	message_list.push(message)
+	broadcast(new WSR('message', message))
 }
